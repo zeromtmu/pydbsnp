3f805dd79932df2f6a897f5d7a17bcd56929c976
anthony-aylward <aaylward@eng.ucsd.edu>
2019-06-27 01:03:32 -0700

    test

--- pydbsnp/query.py
+++ pydbsnp/query.py
@@ -29,6 +29,8 @@ COORD_REGEX = re.compile('.+:[0-9]+$')
 
 # Functions ====================================================================
 
+def rsid_to_coordinates
+
 def parse_arguments():
     parser = ArgumentParser(description='query dbSNP VCF data')
     parser.add_argument(
@@ -50,16 +52,21 @@ def parse_arguments():
 def main():
     args = parse_arguments()
     print(VariantFile(BUILD_TO_VCF[args.reference_build]).header)
-    tbx = TabixFile(BUILD_TO_VCF[args.reference_build])
+    vcf = TabixFile(BUILD_TO_VCF[args.reference_build])
+    rsid = TabixFile(BUILD_TO_RSID[args.reference_build])
+    def rsid_to_coordinates(rsid):
+        rs_number = rsid.replace('rs', '')
+        for row in rsid.fetch('rs', rs_number)
+            yield row[2], int(row[3])
     for variant in args.variants:
         if COORD_REGEX.match(variant):
             chrom, pos = variant.split(':')
             pos = int(pos)
-            for row in tbx.fetch(chrom, pos, pos):
+            for row in vcf.fetch(chrom, pos, pos):
                 print(str(row))
         elif RSID_REGEX.match(variant):
             for chrom, pos in rsid_to_coordinates(variant):
-                for row in tbx.fetch(chrom, pos, pos):
+                for row in vcf.fetch(chrom, pos, pos):
                     print(str(row))
         else:
             raise RuntimeError('Improperly formatted query')
