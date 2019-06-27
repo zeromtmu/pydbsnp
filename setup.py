dc1c0dfd61fd544b578c73492c69b3f3499ebf19
anthony-aylward <aaylward@eng.ucsd.edu>
2019-06-27 01:04:45 -0700

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pydbsnp',
-    version='0.0.9',
+    version='0.0.10',
     author='Anthony Aylward',
     author_email='aaylward@eng.ucsd.edu',
     description='Interface with dbSNP VCF data',
