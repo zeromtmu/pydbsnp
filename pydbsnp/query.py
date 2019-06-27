9e37a1c5c408cf8f45d23a39100f48b366f6b3e8
anthony-aylward <aaylward@eng.ucsd.edu>
2019-06-27 00:54:19 -0700

    update

--- pydbsnp/query.py
+++ pydbsnp/query.py
@@ -59,7 +59,7 @@ def main():
             for row in tbx.fetch(chrom, pos, pos):
                 print(str(row))
         elif RSID_REGEX.match(variant):
-            for coord in rsid_to_coordinates():
+            for coord in rsid_to_coordinates(variant):
         else:
             raise RuntimeError('Improperly formatted query')
     pass
