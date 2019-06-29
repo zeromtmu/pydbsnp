0e38f60fa5944da54ae1e1dd10a3dd2fa8ba50b4
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-06-28 18:08:33 -0700

    export chrom to hgvs

--- pydbsnp/__init__.py
+++ pydbsnp/__init__.py
@@ -1,4 +1,4 @@
 """Interface with dbSNP VCF data"""
 
 from pydbsnp.env import VCF_GRCH37, VCF_GRCH38, RSID_GRCH37, RSID_GRCH38
-from pydbsnp.query import Variant, GeneralizedVariant
+from pydbsnp.query import CHROM_TO_HGVS, Variant, GeneralizedVariant
